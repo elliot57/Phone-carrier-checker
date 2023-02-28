71f6d7207a65725d9f3930eadae5f01266926558
elliott57 <yb2873482734287@proton.me>
2023-02-28 15:28:13 +0000

    .

--- checker.py
+++ checker.py
@@ -4,27 +4,26 @@ def phone_list():
     try:
         with open('phones.txt', 'r') as file:
             file = file.readlines()
-        phones = [s.rstrip()for s in file]
+        phones = [string.rstrip()for string in file]
         return phones
     except FileNotFoundError:
         print("File Not Found")
 
 def check(phones=phone_list()):
-    API_KEY = str(input('Enter your api key: '))
+    API_KEY = str(input('Enter your API Key: '))
     req = requests.session()
-    flag = len(phones)
-    while flag > 1:
-        try:
-            for i in phones:
-            #print(i)
-                resp = req.get(f'https://api.veriphone.io/v2/verify?phone=%2B{i}&key={API_KEY}')
-                flag -= 1
-                resp = resp.json()
-                carrier = resp["carrier"]
-                status = resp["status"]
-                print(f"Status: {status} | {carrier} : {i}")
-        except Exception:
-            pass
+    length_of_phones = len(phones)   
+    try:
+        for each_phone in phones:
+            resp = req.get(f'https://api.veriphone.io/v2/verify?phone=%2B{each_phone}&key={API_KEY}')
+            length_of_phones -= 1
+            resp = resp.json()
+            carrier = resp["carrier"]
+            status = resp["status"]
+            print(f"Status: {status} | {carrier} : {each_phone}")
+    except Exception:
+        pass
 
 if __name__ == "__main__":
     check()
+
